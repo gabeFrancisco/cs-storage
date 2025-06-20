af5f7b1d530efa7ba1d93fc6b67023614f26d1bd
gabeFrancisco <gabegit2@gmail.com>
2025-06-20 20:57:33 -0300

    implemented AddInfrastructure method on CsStorage.IoC

--- CsStorage.IoC/DependencyInjection.cs
+++ CsStorage.IoC/DependencyInjection.cs
@@ -1,8 +1,22 @@
-﻿namespace CsStorage.IoC
-{
-    public class DependencyInjection
-    {
+﻿using CsStorage.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
+namespace CsStorage.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AppDbContext>(options =>
+                options.UseNpgsql(configuration.GetConnectionString("DefaultConnection"),
+
+                b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName))
+            );
+
+            return services;
+        }
     }
 
 }
