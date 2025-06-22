06dad31a1876843f92d64142910b65da8bbe3be6
gabeFrancisco <gabegit2@gmail.com>
2025-06-21 22:46:30 -0300

    created DebtDTO

---
+++ CsStorage.Application/DTOs/DebtDTO.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CsStorage.Application.DTOs
+{
+    public class DebtDTO
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "The Value field is required!")]
+        [Column(TypeName = "decimal(10,2)")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
+        [DataType(DataType.Currency)]
+        [DisplayName("Value")]
+        public decimal Value { get; private set; }
+
+        [Required(ErrorMessage = "The Forecast field is required!")]
+        [DisplayName("Forecast")]
+        public DateTime Forecast { get; private set; }
+
+        [Required(ErrorMessage = "The PaidDate field is required!")]
+        [DisplayName("PaidDate")]
+        public DateTime? PaidDate { get; private set; }
+        public CustomerDTO CustomerDto { get; set; }
+
+        [DisplayName("Customer")]
+        public int CustomerId { get; set; }
+    }
+}
