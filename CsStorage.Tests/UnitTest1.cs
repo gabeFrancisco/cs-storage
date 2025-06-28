7138fa6781fc0b4227944030f28628705c566873
gabeFrancisco <gabegit2@gmail.com>
2025-06-27 23:02:11 -0300

    created CsStorage.Tests project

--- CsStorage.Tests/UnitTest1.cs
+++ CsStorage.Tests/UnitTest1.cs
@@ -1,10 +1,31 @@
-﻿namespace CsStorage.Tests;
+﻿using CsStorage.Application.DTOs;
+using CsStorage.Application.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CsStorage.Tests;
 
 public class UnitTest1
 {
-    [Fact]
-    public void Test1()
+    private readonly IServiceProvider _serviceProvider;
+    public UnitTest1()
     {
+        var services = new ServiceCollection();
 
+        _serviceProvider = services.BuildServiceProvider();
+    }
+    [Fact]
+
+    public void CreateDebt_ShouldReturnDebt()
+    {
+        var debtService = _serviceProvider.GetService<IDebtService>();
+        var result = debtService.Create(new DebtDTO
+        {
+            CustomerId = 1,
+            PaidDate = DateTime.UtcNow,
+            Forecast = DateTime.UtcNow.AddDays(3),
+            Value = 70
+        });
+
+        Assert.NotNull(result);
     }
 }
