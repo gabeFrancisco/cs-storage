57d2aeee4ccd01d84c4aec06ea2638ec9abb0aec
gabeFrancisco <gabegit2@gmail.com>
2025-07-04 12:56:29 -0300

    created CsStorage.Api project

---
+++ CsStorage.Api/Program.cs
@@ -0,0 +1,27 @@
+using CsStorage.IoC;
+
+var builder = WebApplication.CreateBuilder(args);
+var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+builder.Services.AddInfrastructure(configuration);
+builder.Services.AddControllers();
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
