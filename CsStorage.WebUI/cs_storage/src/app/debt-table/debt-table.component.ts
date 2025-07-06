fac2ece9d133e68a7f02c971de99ead9631f3edd
gabeFrancisco <gabegit2@gmail.com>
2025-07-06 15:16:28 -0300

    created debt-service and debt-table list implementation

--- CsStorage.WebUI/cs_storage/src/app/debt-table/debt-table.component.ts
+++ CsStorage.WebUI/cs_storage/src/app/debt-table/debt-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Debt } from '../../models/Debt';
+import { DebtService } from '../debt.service';
 
 @Component({
   selector: 'app-debt-table',
@@ -7,8 +9,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrl: './debt-table.component.css'
 })
 export class DebtTableComponent implements OnInit{
+  debts: Debt[] = [];
+
+  constructor(private debtService: DebtService) { }
+
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.fetchList();
   }
 
+  fetchList(){
+    this.debtService.getDebts().subscribe((res) => {
+      this.debts = res;
+    })
+  }
 }
---
+++ CsStorage.WebUI/cs_storage/src/app/debt.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Debt } from '../models/Debt';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DebtService {
+  private url = "http://localhost:5103/api/debts";
+  constructor(private http: HttpClient) { }
+
+  getDebts(): Observable<Debt[]>{
+    return this.http.get<Debt[]>(this.url);
+  }
+}
