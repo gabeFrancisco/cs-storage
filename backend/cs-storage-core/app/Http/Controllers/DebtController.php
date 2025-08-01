7b10190eb14fc068e08c35bbb55076e2cd911469
gabeFrancisco <gabegit2@gmail.com>
2025-08-01 14:48:14 -0300

    first test with relational data made with success on DebtController

--- backend/cs-storage-core/app/Http/Controllers/DebtController.php
+++ backend/cs-storage-core/app/Http/Controllers/DebtController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Customer;
 use App\Models\Debt;
+use App\Models\Address;
 use Illuminate\Http\Request;
 
 class DebtController extends Controller
@@ -15,17 +17,45 @@ class DebtController extends Controller
 
     public function post(Request $request)
     {
+        $address = null;
+
+        if (!empty($request->input('address'))) {
+            $address = Address::create([
+                'road' => $request->input('road'),
+                'number' => $request->input('number'),
+                'complement' => $request->input('complement'),
+                'neighborhood' => $request->input('neighborhood'),
+                'city' => $request->input('city'),
+                'state' => $request->input('state')
+            ]);
+        }
+
+        $customer = Customer::create([
+            'name' => $request->input('customer.name'),
+            'phone' => $request->input('customer.phone'),
+            'cpf_cnpj' => $request->input('customer.cpf_cnpj'),
+
+        ]);
+
+        if($address){
+            $customer->address()->associate($address);
+        }
+
         $value = $request->input('value');
         $forecast = $request->input('forecast');
 
-        $debt = Debt::create([
+        $debt = new Debt([
             'value' => $value,
             'forecast' => $forecast,
-            'paid_date' => now()->toString()
+            'paid_date' => now()->toString(),
         ]);
 
-        $customer = $debt->customer()->create([
+        $debt->customer()->associate($customer);
+        $debt->save();
 
-        ]);
+        return response()->json([
+            "message" =>"Debt created successfuly",
+            "result" => $debt
+        ], 200);
     }
 }
