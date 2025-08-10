fe77ae5ce49ba775e6b4356c7915f52b1ec360ce
gabeFrancisco <gabegit2@gmail.com>
2025-08-09 23:41:05 -0300

    added cache to missing-products-service to prevent unecesary re-renders and api calls

--- ui/cs_storage/src/app/services/missing-product.service.ts
+++ ui/cs_storage/src/app/services/missing-product.service.ts
@@ -1,13 +1,14 @@
+import { MissingProduct } from './../../models/MissingProduct';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { MissingProduct } from '../../models/MissingProduct';
+import { BehaviorSubject, Observable, shareReplay, tap } from 'rxjs';
 import { ModalType } from '../../utils/modalType';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MissingProductService {
+  private list$?: Observable<MissingProduct[]>;
 
   constructor(private http: HttpClient) { }
   private url = "http://127.0.0.1:8000/api/missingproducts";
@@ -23,30 +24,40 @@ export class MissingProductService {
   private missingProductId = new BehaviorSubject<number | null>(null);
   missingProductId$ = this.missingProductId.asObservable();
 
-  openMissingProductModal() { this.missingProductModalState.next(true)}
-  closeMissingProductModal() { this.missingProductModalState.next(false)}
+  openMissingProductModal() { this.missingProductModalState.next(true) }
+  closeMissingProductModal() { this.missingProductModalState.next(false) }
 
   setModalTypeToCreate() { this.missingProductModalType.next(ModalType.CREATE) }
   setModalTypeToUpdate() { this.missingProductModalType.next(ModalType.UPDATE) }
   setModalTypeToRead() { this.missingProductModalType.next(ModalType.READ) }
 
-  setMissingProductId(id: number){
+  setMissingProductId(id: number) {
     this.missingProductId.next(id);
   }
 
   private refreshList = new BehaviorSubject<void>(undefined);
   refreshList$ = this.refreshList.asObservable();
 
-  triggerUpdate(){
+  triggerUpdate() {
     this.refreshList.next();
   }
 
   //API calls
-  getMissingProducts(): Observable<MissingProduct[]>{
-    return this.http.get<MissingProduct[]>(this.url);
+  getMissingProducts(): Observable<any> {
+    if (!this.list$) {
+      this.list$ = this.http.get<MissingProduct[]>(this.url).pipe(
+        shareReplay(1)
+      )
+    }
+    return this.list$;
   }
 
-  createMissingProduct(payload: MissingProduct): Observable<any>{
+  clearCache() {
+    this.list$ = undefined;
+  }
+
+  createMissingProduct(payload: MissingProduct): Observable<any> {
+    this.clearCache();
     return this.http.post(this.url, payload);
   }
 }
