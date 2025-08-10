e70f12eaa9a2ac9fe4eaea6b07600a653e2616c0
gabeFrancisco <gabegit2@gmail.com>
2025-08-10 19:24:51 -0300

    refactored MissingProductService

--- backend/cs-storage-core/app/Services/MissingProductService.php
+++ backend/cs-storage-core/app/Services/MissingProductService.php
@@ -3,25 +3,48 @@
 namespace App\Services;
 
 use App\Http\Requests\MissingProductRequest;
+use App\Models\Customer;
 use App\Models\MissingProduct;
 
 class MissingProductService
 {
     public function getAll()
     {
-        return MissingProduct::all();
+        return MissingProduct::with('customer')->get();
+    }
+
+    public function getById($id)
+    {
+        return MissingProduct::with('customer')->findOrFail($id);
     }
 
     public function create(MissingProductRequest $request)
     {
-        if(!empty($request)){
-            $missingProduct = MissingProduct::create([
-                'name' => $request->input('name'),
-                'needed_day' => $request->input('needed_day'),
-                'is_bought' => $request->input('is_bought'),
-                'image_url' => $request->input('image_url')
+        $customer = null;
+        if (
+            !empty($request->input('customer.name'))
+            ||
+            !empty($request->input('customer.phone'))
+        ) {
+            $customer = Customer::create([
+                'name' => $request->input('customer.name'),
+                'phone' => $request->input('customer.phone')
             ]);
         }
+
+        $missingProduct = new MissingProduct([
+            'name' => $request->input('name'),
+            'needed_day' => $request->input('needed_day'),
+            'is_bought' => $request->input('is_bought'),
+            'image_url' => $request->input('image_url')
+        ]);
+
+        if ($customer) {
+            $missingProduct->customer()->associate($customer);
+        }
+
+
+        $missingProduct->save();
 
         return $missingProduct;
     }
