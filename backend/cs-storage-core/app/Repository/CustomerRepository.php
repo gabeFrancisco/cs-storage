318f2c2d15e742be8b553d496c629d56a2a766d6
gabeFrancisco <gabegit2@gmail.com>
2025-08-15 12:48:24 -0300

    created getAllCustomers method on CustomerRepository

--- backend/cs-storage-core/app/Repository/CustomerRepository.php
+++ backend/cs-storage-core/app/Repository/CustomerRepository.php
@@ -1,5 +1,23 @@
 <?php
 
-class CustomerRepository{
+namespace App\Repository;
 
+use App\Models\Customer;
+use Illuminate\Support\Facades\DB;
+
+class CustomerRepository{
+    public function getAllCustomers(){
+        $customers = DB::(
+            'select id, created_at, updated_at, name, phone, cpf_cnpj, address_id
+                from customers
+                left join addresses on customers.address_id = addresses.id
+            '
+        );
+
+        return $customers;
+    }
+
+    public function createCustomer(Customer $customer){
+
+    }
 }
