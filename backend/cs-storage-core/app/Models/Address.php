94e411a4d3a872c11e8fb0e28ad4dfcae22a61b2
gabeFrancisco <gabegit2@gmail.com>
2025-08-14 23:28:51 -0300

    started Address, Customer and Debt repositories

--- backend/cs-storage-core/app/Models/Address.php
+++ backend/cs-storage-core/app/Models/Address.php
@@ -6,6 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Address extends Model
 {
+    public $id;
+    public $road;
+    public $number;
+    public $complement;
+    public $neighborhood;
+    public $city;
+    public $state;
+    public $created_at;
+    public $updated_at;
+
+    public function __construct($id, $road, $number, $complement, $neighborhood, $city, $state, $created_at) {
+        $this->id = $id;
+        $this->road = $road;
+        $this->number = $number;
+        $this->neighborhood = $neighborhood;
+        $this->city = $city;
+        $this->state = $state;
+        $this->created_at = $created_at;
+    }
+
     protected $table = 'addresses';
 
     protected $fillable = [
---
+++ backend/cs-storage-core/app/Repository/AddressRepository.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Repository;
+
+use App\Models\Address;
+use Illuminate\Support\Facades\DB;
+
+class AddressRepository
+{
+    public function getAllAddresses()
+    {
+        $addresses = DB::select(
+            'select id, created_at, updated_at, road, number, complement, neighborhood, city, state from addressess'
+        );
+
+        return $addresses;
+    }
+
+    public function createAddress(Address $address)
+    {
+        $address = DB::selectOne(
+            'insert into addressess(created_at, road, number, complement, neighborhood, city, state)
+                    values (?,?,?,?,?,?,?) returning *',
+            [
+                $address->created_at,
+                $address->road,
+                $address->number,
+                $address->complement,
+                $address->neighborhood,
+                $address->city,
+                $address->state
+            ]
+        );
+
+        return $address;
+    }
+}
