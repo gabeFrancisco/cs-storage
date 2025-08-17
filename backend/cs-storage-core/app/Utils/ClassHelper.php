7f6dba2646c24ed4f8d91b405c9dcaaedbfa8a1d
gabeFrancisco <gabegit2@gmail.com>
2025-08-17 18:10:50 -0300

    created ClassHelper class

---
+++ backend/cs-storage-core/app/Utils/ClassHelper.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Utils;
+
+class ClassHelper
+{
+    public static function fillFromSql(object $row, string $class, string $prefix)
+    {
+        $instance = new $class();
+
+        foreach (get_object_vars($row) as $field => $value) {
+
+            //If the SQL result has a model prefix field like customer_ or address_
+            if ($prefix && str_starts_with($field, $prefix)) {
+                $prop = substr($field, strlen($prefix));
+
+                //Check if the prefix field exists on actual model class
+                if (property_exists($instance, $prop)) {
+                    $instance->$prop = $value;
+                }
+                elseif(!$prefix && property_exists($instance, $field)){
+                    $instance->$field = $value;
+                }
+            }
+        }
+        return $instance;
+    }
+}
