6379cbaf1177335b93d1d12f71f104c484fe3d99
gabeFrancisco <gabegit2@gmail.com>
2025-10-17 14:28:11 -0300

    finished Estimate migration

--- backend/cs-storage-core/database/migrations/2025_10_16_155843_estimate.php
+++ backend/cs-storage-core/database/migrations/2025_10_16_155843_estimate.php
@@ -11,10 +11,35 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create("estimate_products", function(Blueprint $table){
+        Schema::create("estimates", function(Blueprint $table){
+            $table->id();
+            $table->timestamps();
+            $table->string("title");
+            $table->string("observations")->nullable();
+            $table->decimal('total');
+
+            $table->integer('customer_id');
+            $table->foreign('customer_id')
+                ->references('id')
+                ->on('customers')
+                ->onDelete('cascade');
+        });
+
+        Schema::create("estimate_items", function(Blueprint $table){
             $table->id();
             $table->timestamps();
             $table->string("name");
+            $table->string("description")->nullable();
+            $table->integer("quantity");
+            $table->decimal("price");
+            $table->integer("product_type");
+            $table->decimal("total");
+
+            $table->integer("estimate_id");
+            $table->foreign("estimate_id")
+                ->references('id')
+                ->on('estimates')
+                ->onDelete('cascade');
         });
     }
 
