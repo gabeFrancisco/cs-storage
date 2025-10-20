bd7aab309b28e17aab89d760995f028c29940a5b
gabeFrancisco <gabegit2@gmail.com>
2025-10-20 16:53:27 -0300

    started test with PHPUnit

---
+++ backend/cs-storage-core/tests/Unit/EstimateTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Repository\EstimateRepository;
+use PHPUnit\Framework\TestCase;
+
+class EstimateTest extends TestCase
+{
+      public function test_example(): void
+    {
+        //TODO
+        $service = EstimateRepository::class;
+
+        $this->assertTrue(true);
+    }
+}
