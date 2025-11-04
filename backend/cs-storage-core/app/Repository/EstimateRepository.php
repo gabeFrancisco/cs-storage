352b563c1c7ee7ff6744dfd59d2d60aa2da4b967
gabeFrancisco <gabegit2@gmail.com>
2025-11-04 14:40:31 -0300

    started getAll method nad sql query on EstimateRepository

--- backend/cs-storage-core/app/Repository/EstimateRepository.php
+++ backend/cs-storage-core/app/Repository/EstimateRepository.php
@@ -20,6 +20,30 @@ class EstimateRepository
         $this->customerRepository = $customerRepository;
     }
 
+    private $selectAllWithJoinQuery =
+        'SELECT e.id        AS e_id,
+            e.title         AS e_title,
+            e.observations  AS e_observations,
+            e.total         AS e_total,
+            e.customer_id   AS e_customer_id,
+            e.created_at    AS e_created_at,
+            e.updated_at    AS e_updated_at,
+            c.id            AS c_id,
+            c.name          AS c_name,
+            c.phone         AS c_phone,
+            c.cpf_cnpj      AS c_cpf_cnpj,
+            c.created_at    AS c_created_at,
+            c.updated_at    AS c.updated_at
+            ei.id           AS ei_id,
+            ei.name         AS ei_name,
+            ei.description  AS ei_description,
+            ei.quantity     AS ei_quantity,
+            ei.price        AS ei_price,
+            ei.product_type AS ei_product_type,
+            ei.total        AS ei_total,
+            ei.estimate_id  AS ei_estimate_id
+            ';
+
     public function createEstimate(Estimate $estimate)
     {
 
@@ -54,5 +78,10 @@ class EstimateRepository
             DB::rollBack();
             throw $e;
         }
+    }
+
+    public function getAllEstimates()
+    {
+
     }
 }
