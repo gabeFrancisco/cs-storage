6f2149488ca3d49a22399da35430362ed6f8f77b
gabeFrancisco <gabegit2@gmail.com>
2025-12-10 20:38:43 -0300

    created EstimateMapper class

--- backend/cs-storage-core/app/Mappers/EstimateMapper.php
+++ backend/cs-storage-core/app/Mappers/EstimateMapper.php
@@ -2,13 +2,18 @@
 
 namespace App\Mappers;
 
+use App\Models\Customer;
 use App\Models\Estimate;
 
 class EstimateMapper{
     public static function fromSql($data){
         $estimate = new Estimate();
+        $customer = new Customer();
 
-        $estimate->title = $data['e_title'];
+        $estimate->title = $data->e_title;
+        $customer->name = "John Frank";
+
+        $estimate->customer = $customer;
 
         return $estimate;
     }
