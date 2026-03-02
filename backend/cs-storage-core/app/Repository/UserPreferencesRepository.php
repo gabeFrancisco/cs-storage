e4255fdb74650202867d60aeaa3b173688f7a56e
gabefrancisco <gabegit2@gmail.com>
2026-03-01 22:00:35 -0300

    created createUserPreferences method on UserPreferencesRepository

--- backend/cs-storage-core/app/Repository/UserPreferencesRepository.php
+++ backend/cs-storage-core/app/Repository/UserPreferencesRepository.php
@@ -2,6 +2,7 @@
 
 namespace App\Repository;
 
+use App\Models\UserPreferences;
 use DB;
 
 class UserPreferencesRepository
@@ -18,5 +19,21 @@ class UserPreferencesRepository
     {
         $user_preferences = DB::select($this->selectQuery . ' WHERE user_id = ?', [$user_id]);
         return $user_preferences;
+    }
+
+    public function createUserPreferences(UserPreferences $userPreferences)
+    {
+        $dbUserPreferences = DB::selectOne(
+            'INSERT INTO user_preferences(log_email, email_interval, user_id, created_at)
+            VALUES (?,?,?,?) returning *;',
+            [
+                $userPreferences->log_email,
+                $userPreferences->email_interval,
+                $userPreferences->user_id,
+                $userPreferences->created_at
+            ]
+        );
+
+        return $dbUserPreferences;
     }
 }
