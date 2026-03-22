25708934f7fddcc26d54fb8c100d9a99a4dc0ac1
gabefrancisco <gabegit2@gmail.com>
2026-03-22 01:01:59 -0300

    refactored Address to an Eloquent model. Removed AddressRepository

--- backend/cs-storage-core/app/Models/Address.php
+++ backend/cs-storage-core/app/Models/Address.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-class Address extends BaseModel
+use Illuminate\Database\Eloquent\Model;
+
+class Address extends Model
 {
-    public $road;
-    public $number;
-    public $complement;
-    public $neighborhood;
-    public $city;
-    public $state;
+    // public $road;
+    // public $number;
+    // public $complement;
+    // public $neighborhood;
+    // public $city;
+    // public $state;
+
+    protected $fillable = [
+        'road',
+        'number',
+        'complement',
+        'neighborhood',
+        'city',
+        'state'
+    ];
 }
