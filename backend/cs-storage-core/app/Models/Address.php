c0732ecfb72f6800f33e0f6269fb24df1e704a41
gabefrancisco <gabegit2@gmail.com>
2026-03-23 00:09:26 -0300

    refactored MissingProduct to an Eloquent model. added phpDocBlocks

--- backend/cs-storage-core/app/Models/Address.php
+++ backend/cs-storage-core/app/Models/Address.php
@@ -4,6 +4,31 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $road
+ * @property string|null $number
+ * @property string|null $complement
+ * @property string|null $neighborhood
+ * @property string|null $city
+ * @property string|null $state
+ * @property-read \App\Models\Customer|null $customer
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereComplement($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereNeighborhood($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereRoad($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereState($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Address whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Address extends Model
 {
     protected $fillable = [
--- backend/cs-storage-core/app/Models/Customer.php
+++ backend/cs-storage-core/app/Models/Customer.php
@@ -5,6 +5,29 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 
+/**
+ * @property int $id
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string $name
+ * @property string $phone
+ * @property string|null $cpf_cnpj
+ * @property int|null $address_id
+ * @property-read \App\Models\Address|null $address
+ * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Debt> $debts
+ * @property-read int|null $debts_count
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer query()
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereAddressId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereCpfCnpj($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder<static>|Customer whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Customer extends Model
 {
     protected $fillable = [
