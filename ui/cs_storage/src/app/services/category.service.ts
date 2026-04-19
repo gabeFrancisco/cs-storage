8781248549146fb9ddd6cd82cbe831682e62b0e6
gabefrancisco <gabegit2@gmail.com>
2026-04-19 18:59:03 -0300

    implemented getCategories in category-service(angular)

--- ui/cs_storage/src/app/services/category.service.ts
+++ ui/cs_storage/src/app/services/category.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, shareReplay } from 'rxjs';
 import { Category } from '../../models/Category';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
@@ -11,6 +11,20 @@ export class CategoryService {
   private list$?: Observable<Category[]>;
 
   constructor(private http: HttpClient){ }
-  private ulr = `${environment.apiUrl}/categories`;
+  private url = `${environment.apiUrl}/categories`;
 
+  private refreshList = new BehaviorSubject<void>(undefined);
+  refreshList$ = this.refreshList.asObservable();
+
+  triggerUpdate(){
+    this.refreshList.next();
+  }
+
+  getCategories(): Observable<Category[]>{
+    return this.http.get<Category[]>(this.url).pipe(shareReplay(1))
+  }
+
+  clearCache(){
+    this.list$ = undefined;
+  }
 }
