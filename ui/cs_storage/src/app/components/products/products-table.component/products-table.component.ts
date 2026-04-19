4322e708fd40bfe820fff6aa754039ec35e11e1e
gabefrancisco <gabegit2@gmail.com>
2026-04-18 21:53:35 -0300

    implemented product-table component

--- ui/cs_storage/src/app/components/products/products-table.component/products-table.component.ts
+++ ui/cs_storage/src/app/components/products/products-table.component/products-table.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { Product } from '../../../../models/Product';
 
 @Component({
-  selector: 'app-products-table.component',
+  selector: 'app-products-table',
   standalone: false,
   templateUrl: './products-table.component.html',
   styleUrl: './products-table.component.css',
 })
 export class ProductsTableComponent {
+  products: Product[] = [];
+  loading = true;
 
+  fetchList(){}
+  openProductModal(){}
+  openProductUpdateModal(id: number){}
+  deleteProductModal(id: number){}
 }
