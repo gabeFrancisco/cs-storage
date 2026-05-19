87b24f05d1dbf9958a6e10ec7ce7b5b57885a86e
gabefrancisco <gabegit2@gmail.com>
2026-05-19 14:06:57 -0300

    refactored CashRegister model and cash-register-table component

--- ui/cs_storage/src/models/CashRegister.ts
+++ ui/cs_storage/src/models/CashRegister.ts
@@ -1,10 +1,12 @@
 import { BaseEntity } from "./BaseEntity";
 import { PaymentType } from "./enums/PaymentType";
+import { Product } from "./Product";
 
 export interface CashRegister extends BaseEntity {
   value: number;
   quantity: number;
   product_id: number;
+  product: Product;
   description: string;
   payment_type: PaymentType;
 }
