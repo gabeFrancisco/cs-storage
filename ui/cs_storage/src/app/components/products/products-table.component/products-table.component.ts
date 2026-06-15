c143189c118ad9bf6633a49eaeaa930256056fee
gabefrancisco <gabegit2@gmail.com>
2026-06-15 14:42:50 -0300

    started product search debounce input

--- ui/cs_storage/src/app/components/products/products-table.component/products-table.component.ts
+++ ui/cs_storage/src/app/components/products/products-table.component/products-table.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Product } from '../../../../models/Product';
 import { ProductService } from '../../../services/product.service';
 import { ProductType } from '../../../../models/enums/ProductType';
+import { FormControl } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-products-table',
@@ -13,18 +15,29 @@ export class ProductsTableComponent implements OnInit {
   products: Product[] = [];
   loading = true;
 
-  parseProductType(id: number){
+  search = new FormControl("");
+
+  parseProductType(id: number) {
     return ProductType[id];
   }
 
-  constructor(private productService: ProductService){}
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
     this.productService.triggerUpdate();
     this.productService.refreshList$.subscribe(() => this.fetchList())
+
+    this.search.valueChanges
+      .pipe(
+        debounceTime(500),
+        distinctUntilChanged()
+      )
+      .subscribe(value => {
+
+      })
   }
 
-  fetchList(){
+  fetchList() {
     this.productService.getProducts().subscribe({
       next: res => {
         this.products = res
@@ -33,9 +46,9 @@ export class ProductsTableComponent implements OnInit {
     })
   }
 
-  openProductModal(){
+  openProductModal() {
     this.productService.openProductPostModal();
   }
-  openProductUpdateModal(id: number){}
-  deleteProductModal(id: number){}
+  openProductUpdateModal(id: number) { }
+  deleteProductModal(id: number) { }
 }
