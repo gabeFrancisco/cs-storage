23a5641dd974078735c72e52cad08b61db8880d0
gabefrancisco <gabegit2@gmail.com>
2026-07-09 16:02:44 -0300

    started dash-cards implementation

--- ui/cs_storage/src/app/components/dash-cards/dash-cards.ts
+++ ui/cs_storage/src/app/components/dash-cards/dash-cards.ts
@@ -1,4 +1,12 @@
 import { Component } from '@angular/core';
+import { faCashRegister, faDollarSign, IconDefinition } from '@fortawesome/free-solid-svg-icons';
+
+interface CardItem {
+  icon: IconDefinition,
+  name: string
+  data?: number
+  class: string
+}
 
 @Component({
   selector: 'app-dash-cards',
@@ -6,6 +14,23 @@ import { Component } from '@angular/core';
   templateUrl: './dash-cards.html',
   styleUrl: './dash-cards.css',
 })
-export class DashCards {
 
+export class DashCards {
+  faCategory = faDollarSign
+  faDebts = faCashRegister
+
+  items: CardItem[] = [
+    {
+      icon: this.faCategory,
+      name: "Caixa de Hoje",
+      data: 312,
+      class: "bg-emerald-500"
+    },
+    {
+      icon: this.faDebts,
+      name: "Débitos do Mês",
+      data: 712,
+      class: "bg-blue-500"
+    }
+  ]
 }
